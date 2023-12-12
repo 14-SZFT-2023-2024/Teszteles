2cdf2b1b957a527137277b00f138093a95e1c231
Norbert Rácz <norbert.russov@gmail.com>
2023-12-12 11:49:50 +0100

    fele sem működik de az enyém :3

---
+++ ConsoleApp1/Teszteles.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Teszteles
+    {
+
+        public class Csv
+        {
+            public int TaxiId { get; set; }
+            public DateTime Datum { get; set; }
+            public int Hossz { get; set; }
+            public double Tavolsag { get; set; }
+            public double Ar { get; set; }
+            public double Tip { get; set; }
+            public string FizetesiMod { get; set; }
+        }
+
+        private List<Csv> records = new List<Csv>();
+
+        public int ElsoFeladat()
+        {
+            //semmi?
+            return (0);
+        }
+
+        public int MasodikFeladat()
+        {
+            string file = "fuvar.csv";
+          
+            try
+            {
+
+                using (var reader = new StreamReader(file))
+                {
+                    reader.ReadLine();
+
+                    while (!reader.EndOfStream)
+                    {
+
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+
+                        if (values.Length >= 7)
+                        {
+                            Csv record = new Csv
+                            {
+                                TaxiId = int.Parse(values[0]),
+                                Datum = DateTime.ParseExact(values[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+                                Hossz = int.Parse(values[2]),
+                                Tavolsag = double.Parse(values[3].Replace(',', '.'), CultureInfo.InvariantCulture),
+                                Ar = double.Parse(values[4].Replace(',', '.'), CultureInfo.InvariantCulture),
+                                Tip = double.Parse(values[5].Replace(',', '.'), CultureInfo.InvariantCulture),
+                                FizetesiMod = values[6],
+                            };
+
+                            records.Add(record);
+                        }
+                        
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return 1;
+                throw new Exception(ex.Message);
+            }
+
+                return 0;
+        }
+
+       public int HarmadikFeladat()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+            int fuvarok = 0;
+
+            foreach (var record in records)
+            {
+                fuvarok++;
+            }
+
+
+            return fuvarok;
+        }
+
+        public double NegyedikFeladatTaxi()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            double bevetel = 0;
+            int taxiid = 6185;
+            for(int i = 0; i< records.Count(); i++)
+            {
+                if (records[i].TaxiId == taxiid)
+                {
+                    bevetel += records[i].Ar + records[i].Tip;
+                }
+            }
+
+            return bevetel;
+        }
+        
+        public int NegyedikFeladatFuvar()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            int taxiid = 6185;
+            int fuvarok = 0;
+            for(int i = 0; i< records.Count(); i++)
+            {
+                if (records[i].TaxiId == taxiid)
+                {
+                    fuvarok++;
+                }
+            }
+
+            return fuvarok;
+        }
+
+        public int OtodikFeladatKartya()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            string FizetesMod = "bankkártya";
+            int count = 0;
+
+            for(int i = 0; i< records.Count(); i++)
+            {
+                if (records[i].FizetesiMod == FizetesMod)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int OtodikFeladatKP()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+            
+            string FizetesMod = "észpénz";
+            int count = 0;
+
+            for (int i = 0; i < records.Count(); i++)
+            {
+                if (records[i].FizetesiMod == FizetesMod)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int OtodikFeladatVitatott()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            string FizetesMod = "vitatott";
+            int count = 0;
+
+            for (int i = 0; i < records.Count(); i++)
+            {
+                if (records[i].FizetesiMod == FizetesMod)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int OtodikFeladatIngyen()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            string FizetesMod = "ingyenes";
+            int count = 0;
+
+            for (int i = 0; i < records.Count(); i++)
+            {
+                if (records[i].FizetesiMod == FizetesMod)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int OtodikFeladatIsmeretlen()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            string FizetesMod = "ismeretlen";
+            int count = 0;
+
+            for (int i = 0; i < records.Count(); i++)
+            {
+                if (records[i].FizetesiMod == FizetesMod)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public double HatodikFeladat()
+        {
+            Teszteles ts = new Teszteles();
+            ts.MasodikFeladat();
+
+            double tav = 0;
+
+            for (int i = 0; i < records.Count(); i++)
+            {
+                tav = records[i].Tavolsag * 1.6;
+            }
+
+
+            return tav;
+        }
+
+        public static void Main(string[] args)
+        {
+            Teszteles teszteles = new Teszteles();
+            teszteles.ElsoFeladat();
+            teszteles.MasodikFeladat();
+            teszteles.HarmadikFeladat();
+            teszteles.NegyedikFeladatFuvar();
+            teszteles.NegyedikFeladatTaxi();
+            teszteles.OtodikFeladatIngyen();
+            teszteles.OtodikFeladatVitatott();
+            teszteles.OtodikFeladatKartya();
+            teszteles.OtodikFeladatIsmeretlen();
+            teszteles.OtodikFeladatKP();
+            teszteles.HatodikFeladat();
+        }
+        
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+using FluentAssertions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /*[TestMethod]
+        public void TesztOsszead()
+        {
+            Teszteles ts = new Teszteles();
+            int result = ts.Osszead(1, 1);
+            Assert.AreEqual(2, result);
+        }
+        */
+
+
+        [TestMethod]
+        public void ElsoTeszt()
+        {
+            Teszteles ts = new Teszteles();
+            int result = ts.ElsoFeladat();
+            result.Should().Be(0);
+        }
+        [TestMethod]
+        public void MasodikTeszt()
+        {
+            Teszteles ts = new Teszteles();
+            int result = ts.MasodikFeladat();
+            result.Should().Be(0);
+        }
+        [TestMethod]
+        public void HarmadikTeszt()
+        {
+            Teszteles ts = new Teszteles();
+            int result = ts.HarmadikFeladat();
+            result.Should().Be(1859);
+        }
+        [TestMethod]
+        public void NegyedikTesztTaxi()
+        {
+            Teszteles ts = new Teszteles();
+            double result1 = ts.NegyedikFeladatTaxi();
+            result1.Should().Be(33.75);
+        }
+        [TestMethod]
+        public void NegyedikTesztFuvar()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.NegyedikFeladatFuvar();
+            result1.Should().Be(4);
+        }
+        [TestMethod]
+        public void OtodikTesztKartya()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.OtodikFeladatKartya();
+            result1.Should().Be(793);
+        }
+        [TestMethod]
+        public void OtodikTesztKP()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.OtodikFeladatKP();
+            result1.Should().Be(1050);
+        }
+        [TestMethod]
+        public void OtodikTesztVitatott()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.OtodikFeladatVitatott();
+            result1.Should().Be(4);
+        }
+        [TestMethod]
+        public void OtodikTesztIngyen()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.OtodikFeladatIngyen();
+            result1.Should().Be(10);
+        }
+        [TestMethod]
+        public void OtodikTesztIsmeretlen()
+        {
+            Teszteles ts = new Teszteles();
+            int result1 = ts.OtodikFeladatIsmeretlen();
+            result1.Should().Be(2);
+        }
+        [TestMethod]
+        public void HatodikTeszt()
+        {
+            Teszteles ts = new Teszteles();
+            double result = ts.HatodikFeladat();
+            result.Should().Be(8099.95);
+        }
+    }
+}
